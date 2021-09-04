6524905bcf4ac65cb3f2fcb84f2a2f2c3cd70801
Maik <panzenbaby@gmx.de>
2021-09-04 21:01:13 +0200

    init plugin with hello world

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = {
-  // Configuration of the plugin
+  register () {
+    this.routes = [
+      {
+        path: '/stacking-report',
+        name: 'stacking-report',
+        component: 'Report'
+      }
+    ]
+
+    this.menuItems = [
+      {
+        routeName: 'stacking-report',
+        title: 'Stacking Report'
+      }
+    ]
+  },
+
+  getComponentPaths () {
+    return this.routes.reduce((all, route) => {
+      return {
+        ...all,
+        [route.component]: `pages/${route.component}.js`
+      }
+    }, {})
+  },
+
+  getRoutes () {
+    return this.routes
+  },
+
+  getMenuItems () {
+    return this.menuItems
+  }
 }
---
+++ src/pages/Report.js
@@ -0,0 +1,7 @@
+module.exports = {
+  template: `
+    <div class="flex flex-col">
+        <b>Hello World</b>
+    </div>
+    `
+}
